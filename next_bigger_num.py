a0fcb099204084d2e9b0ba80dcf98b8f86e5633a
Khusan Turdiev <husanpy@users.noreply.github.com>
2021-01-10 02:13:41 +0500

    Update next_bigger_num.py

--- next_bigger_num.py
+++ next_bigger_num.py
@@ -8,3 +8,25 @@ def next_bigger(n):
     if listOfNums.index(n) == len(listOfNums) - 1:
         return -1
     return listOfNums[listOfNums.index(n) + 1]
+
+
+#the one below proved to be right, tho I don't know the beneath algorithm. I found it at https://codereview.stackexchange.com/questions/115609/next-bigger-number-with-the-same-digits
+#and it was saying:
+"""
+1. Find largest index i such that array[i − 1] < array[i].
+2. Find largest index j such that j ≥ i and array[j] > array[i − 1].
+3. Swap array[j] and array[i − 1].
+4. Reverse the suffix starting at array[i].
+"""
+#the original link tho: https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+#I slightly modified the code to match the test requirements (I didn't change algorithm, just converted str, int, list and etc. And implemented managing of raised errors
+def next_bigger(n):
+    array = list(map(int, str(n)))
+    try:
+        i = max(i for i in range(1, len(array)) if array[i - 1] < array[i])
+        j = max(j for j in range(i, len(array)) if array[j] > array[i - 1])
+        array[j], array[i - 1] = array[i - 1], array[j]
+        array[i:] = reversed(array[i:])
+        return int(''.join([str(i) for i in array]))
+    except ValueError:
+        return -1
